aa692c1cf1010c9e3e8822125ad3f160349becde
Bariul <musawirbariul@gmail.com>
2021-06-09 19:28:06 +0800

    first commit

---
+++ app/src/main/java/com/bariul/biodata/buatbiodata.java
@@ -0,0 +1,14 @@
+package com.bariul.biodata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class buatbiodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buatbiodata);
+    }
+}
---
+++ app/src/main/java/com/bariul/biodata/ubdatebiodata.java
@@ -0,0 +1,14 @@
+package com.bariul.biodata;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ubdatebiodata extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubdatebiodata);
+    }
+}
